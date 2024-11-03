e524a816fa8f6b089bbdf9712757b24b6333b52a
Artur Sultanov <sultanov.personal@gmail.com>
2024-11-03 14:44:51 +0100

    chore(): update routers

--- backend/src/database/database.py
+++ backend/src/database/database.py
@@ -1,19 +1,12 @@
-from typing import Annotated as typingAnnotated
+from typing import Annotated as typingAnnotated, Generator
 from fastapi import Depends
 from sqlalchemy import create_engine, String
 from sqlalchemy.orm import sessionmaker, DeclarativeBase, Session
 from config.settings import settings
 from src.database.models import Player
 
-engine = create_engine(
-    url=settings.database_url,
-    echo=settings.SQL_ALCHEMY_DEBUG,
-    pool_size=10,
-    max_overflow=10
-)
-
+# Type aliases for convenience
 Str256 = typingAnnotated[str, String(256)]
-
 Str2048 = typingAnnotated[str, String(2048)]
 
 class Base(DeclarativeBase):
@@ -22,13 +15,24 @@ class Base(DeclarativeBase):
         Str2048: String(2048)
     }
 
-class DBSession:
-    def __init__(self) -> None:
-        self.session_factory = sessionmaker(bind=engine)
-    
-    def update_player_login(player_id: int, new_login: Str256):
-        with self.session_factory() as session:
-            player = session.get(Player, player_id)
-            player.login = new_login
-            session.refresh(player)
-            session.commit()
+engine = create_engine(
+    url=settings.database_url,
+    echo=settings.SQL_ALCHEMY_DEBUG,
+    pool_size=10,
+    max_overflow=10
+)
+
+session_factory = sessionmaker(bind=engine)
+
+def get_db_session() -> Generator[Session, None, None]:
+    """
+    Generator to get a session from the session factory
+    """
+    db = session_factory()  # Creates a new session
+    try:
+        yield db  # Yields the session for use in the calling context
+    finally:
+        db.close()  # Ensures the session is closed after use
+
+
+db_dependency = typingAnnotated[Session, Depends(get_db_session)]
