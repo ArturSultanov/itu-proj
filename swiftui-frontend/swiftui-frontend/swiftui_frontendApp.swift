50f46ed1d6896e41ca2bab459491d2fac6fa5e5d
Artur Sultanov <sultanov.personal@gmail.com>
2024-11-07 14:25:16 +0100

    feat(): initial swiftui-frontend structure

---
+++ swiftui-frontend/swiftui-frontend/swiftui_frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_frontendApp.swift
+//  swiftui-frontend
+//
+//  Created by Artur Sultanov on 07.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swiftui-frontend/swiftui-frontendTests/swiftui_frontendTests.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_frontendTests.swift
+//  swiftui-frontendTests
+//
+//  Created by Artur Sultanov on 07.11.2024.
+//
+
+import Testing
+@testable import swiftui_frontend
+
+struct swiftui_frontendTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
