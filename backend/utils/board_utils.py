f31eacabf69d2abd42d6de446f17f38fdfca2927
Artur Sultanov <sultanov.personal@gmail.com>
2024-11-10 11:07:25 +0100

    fix(): remove redudant improt

--- backend/utils/board_utils.py
+++ backend/utils/board_utils.py
@@ -3,7 +3,7 @@ from enum import Enum
 from typing import Tuple, Set, List, Optional
 
 from backend.config import get_heart_recovery_moves
-from backend.models import GemPositionDTO, GameUpdateDTO, GemBase, PlayerDTO, SwapGemsDTO, BordStatusDTO, GameDTO
+from backend.models import GemPositionDTO, GameUpdateDTO, GemBase, PlayerDTO, SwapGemsDTO, GameDTO
 
 
 class GemType(Enum):
