8ee1c79bfe56944e56b49aedc2176c8bb880c8fa
txfedorova <fedorovatse@gmail.com>
2024-12-11 16:23:57 +0100

    login window is OK

--- pyqt-frontend/windows/login_window.py
+++ pyqt-frontend/windows/login_window.py
@@ -7,73 +7,58 @@ class LoginScreen(QWidget):
         super().__init__()
         self.controller = controller
         self.setWindowTitle("Login")
-
-        self.layout = QVBoxLayout()
-
-        # Основной горизонтальный макет
+        
         main_layout = QHBoxLayout()
-
-        # Левый блок с полем ввода и подчеркиванием
         left_layout = QVBoxLayout()
 
-        # Текстовая метка "Enter your name"
         self.label = QLabel("Enter your name")
         self.label.setObjectName("nameTextLabel")
         left_layout.addWidget(self.label)
 
-        # Иконка пользователя
+        user_layout = QHBoxLayout()
+        user_layout.setObjectName("userLayout")
+        # spacer_left = QSpacerItem(0, 0, QSizePolicy.Minimum)
+        # user_layout.addItem(spacer_left)
         self.icon = QSvgWidget("assets/icons/user_icon.svg")
         self.icon.setObjectName("userIcon")
-        self.icon.setFixedSize(90, 100)
-        left_layout.addWidget(self.icon)
+        self.icon.setFixedSize(70, 80)
+        user_layout.addWidget(self.icon)
 
-        # Поле ввода
-        self.username_input = QLineEdit(self)
-        self.username_input.setObjectName("usernameInput")
-        left_layout.addWidget(self.username_input)
+        user_input = QVBoxLayout()
+        self.username = QLineEdit(self)
+        self.username.setObjectName("usernameInput")
+        user_input.addWidget(self.username)
+        self.userpic = QLabel(self)
+        self.userpic.setPixmap(QPixmap("assets/icons/underscore.png"))
+        self.userpic.setObjectName("underscoreIcon")
+        self.userpic.setFixedHeight(10)
+        user_input.addWidget(self.userpic)
+        user_layout.addLayout(user_input)
 
-        # Картинка под полем ввода
-        self.image_label = QLabel(self)
-        self.image_label.setPixmap(QPixmap("assets/icons/underscore.png"))
-        self.image_label.setObjectName("underscoreIcon")
-        self.image_label.setFixedHeight(10)
-        left_layout.addWidget(self.image_label)
+        user_layout.setContentsMargins(110, 0, 0, 0)
+        left_layout.addLayout(user_layout)
 
-        # Добавляем левый блок в основной горизонтальный макет
+        self.login_btn = QPushButton("Confirm", self)
+        self.login_btn.setObjectName("confirmBtn")
+        self.login_btn.clicked.connect(self.on_login_button_click)
+        left_layout.addWidget(self.login_btn)
+
+        self.quit_btn = QPushButton("Quit Game", self)
+        self.quit_btn.setObjectName("quitBtn")
+        self.quit_btn.clicked.connect(self.controller.quit_game)
+        left_layout.addWidget(self.quit_btn)
+
         main_layout.addLayout(left_layout)
 
-        # Добавляем Spacer между блоками
-        spacer = QSpacerItem(50, 0, QSizePolicy.Expanding, QSizePolicy.Minimum)
-        main_layout.addItem(spacer)
+        self.right_img = QLabel(self)
+        self.right_img.setPixmap(QPixmap("assets/icons/login_pic.png")) 
+        self.right_img.setObjectName("loginPic")
+        main_layout.addWidget(self.right_img)
 
-        # Картинка в правой части экрана
-        self.right_image = QLabel(self)
-        self.right_image.setPixmap(QPixmap("assets/icons/login_pic.png")) 
-        self.right_image.setObjectName("loginPic")
-        main_layout.addWidget(self.right_image)
-
-        # Добавляем основной горизонтальный макет в основной вертикальный
-        self.layout.addLayout(main_layout)
-
-        # Кнопка подтверждения
-        self.login_button = QPushButton("Confirm", self)
-        self.login_button.setObjectName("confirmBtn")
-        self.login_button.clicked.connect(self.on_login_button_click)
-        self.layout.addWidget(self.login_button)
-
-        # Кнопка выхода
-        self.quit_button = QPushButton("Quit Game", self)
-        self.quit_button.setObjectName("quitBtn")
-        self.quit_button.clicked.connect(self.controller.quit_game)
-        self.layout.addWidget(self.quit_button)
-
-        # Устанавливаем основной макет
-        self.setLayout(self.layout)
+        self.setLayout(main_layout)
 
     def on_login_button_click(self):
-        username = self.username_input.text()
-        
-        # Запрос к серверу
+        username = self.username.text()
         response = self.controller.login(username)
         if response:
             self.controller.show_main_menu()
